ad84163bd1c2f46898bef567f634ca22692bc9c5
Jakub Wilk <ubanus@users.sf.net>
2008-03-15 19:30:07 +0000

    More complete `sexpr.{cc,hh}`.

---
+++ sexpr.cc
@@ -0,0 +1,31 @@
+/* Copyright © 2008 Jakub Wilk
+ *
+ * This package is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 dated June, 1991.
+ */
+
+#include "sexpr.hh"
+
+static std::ostream *puts_stream = NULL;
+
+static int my_puts(const char *s)
+{
+  *puts_stream << s;
+  return -(!puts_stream->good());
+}
+
+namespace sexpr
+{
+  std::ostream &operator<<(std::ostream &stream, const sexpr::Ref &expr)
+  {
+    int (*old_minilisp_puts)(const char *s) = minilisp_puts;
+    minilisp_puts = my_puts;
+    puts_stream = &stream;
+    miniexp_pprin(expr, 1 << 10);
+    minilisp_puts = old_minilisp_puts;
+    return stream;
+  }
+}
+
+// vim:ts=2 sw=2 et
--- sexpr.hh
+++ sexpr.hh
@@ -1,4 +1,4 @@
-/* Copyright © 2007, 2008 Jakub Wilk
+/* Copyright © 2008 Jakub Wilk
  *
  * This package is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -7,6 +7,8 @@
 
 #ifndef PDF2DJVU_SEXPR_H
 #define PDF2DJVU_SEXPR_H
+
+#include <iostream>
 
 #include <libdjvu/miniexp.h>
 
@@ -19,17 +21,21 @@ namespace sexpr
     Ref(const Ref &ref) { this->var = ref.var; }
     Ref(miniexp_t expr) { this->var = expr; }
     Ref& operator =(miniexp_t expr) { this->var = expr; return *this; }
-    operator miniexp_t&() { return this->var; }
+    operator miniexp_t&() const { return const_cast<Ref*>(this)->var; }
     inline const char * repr()
     {
       return miniexp_to_str(miniexp_pname(this->var, 0));
     }
+    friend std::ostream &operator<<(std::ostream &, const Ref &);
   };
   static inline miniexp_t cons(miniexp_t car, miniexp_t cdr) { return miniexp_cons(car, cdr); }
   static inline miniexp_t symbol(const char *name) { return miniexp_symbol(name); }
+  static inline miniexp_t symbol(const std::string &name) { return miniexp_symbol(name.c_str()); }
   static inline miniexp_t string(const char *value) { return miniexp_string(value); }
   static inline miniexp_t string(const std::string &value) { return miniexp_string(value.c_str()); }
+  static inline miniexp_t integer(int n) { return miniexp_number(n); }
   static const miniexp_t nil = miniexp_nil;
+  static const Ref &empty_string = string("");
 }
 
 #endif
