187084a7e7e40bdb79304f5e37697dea8869563f
Jakub Wilk <ubanus@users.sf.net>
2009-06-13 19:12:15 +0200

    Internalization + Polish translation. Thanks to Matueusz Turcza for providing initial patch.

--- i18n.hh
+++ i18n.hh
@@ -1,5 +1,33 @@
+/* Copyright © 2009 Jakub Wilk
+ *
+ * This package is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 dated June, 1991.
+ */
+
 #ifndef PDF2DJVU_I18N
 #define PDF2DJVU_I18N
+
+#include "version.hh"
+#include "paths.hh"
+
+#ifdef ENABLE_NLS
+
+#include <libintl.h>
+
+static inline const char * _(const char *message_id)
+{
+  return gettext(message_id);
+}
+
+static inline void initialize_i18n()
+{
+  setlocale(LC_ALL, "");
+  bindtextdomain(PACKAGE_NAME, paths::localedir);
+  textdomain(PACKAGE_NAME);
+}
+
+#else
 
 static inline const char * ngettext(const char *message_id, const char *message_id_plural, unsigned long int n)
 {
@@ -15,6 +43,8 @@ static inline void initialize_i18n()
 {
   /* Do nothing. */
 }
+
+#endif
 
 #endif
 
