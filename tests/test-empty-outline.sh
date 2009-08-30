c03cb1da1219efbd2364a95f5a0dd3d72d3507bf
Jakub Wilk <ubanus@users.sf.net>
2009-08-30 23:52:41 +0200

    Add test for empty (but existing) outline.

---
+++ tests/test-empty-outline.sh
@@ -0,0 +1,13 @@
+# Copyright © 2009 Jakub Wilk
+#
+# This package is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 dated June, 1991.
+
+# http://bugs.debian.org/508391
+# Fixed in [].
+
+. common.sh
+
+test_pdf2djvu
+! djvused -e print-outline "$djvu_file" | grep .
