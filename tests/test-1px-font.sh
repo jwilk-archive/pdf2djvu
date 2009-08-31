1fa410bf8e05f1f0720c2669d516995e34a5c158
Jakub Wilk <ubanus@users.sf.net>
2009-08-31 11:24:29 +0200

    Add test for tiny fonts.

---
+++ tests/test-1px-font.sh
@@ -0,0 +1,12 @@
+# Copyright © 2009 Jakub Wilk
+#
+# This package is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 dated June, 1991.
+
+# http://code.google.com/p/pdf2djvu/issues/detail?id=5
+# Fixed in [1a39024ea13a].
+
+. common.sh
+
+test_pdf2djvu
