e211b487e2189e517310a79141f2f1f430ec0e23
Jakub Wilk <ubanus@users.sf.net>
2009-08-31 13:20:38 +0200

    Add test for a rotated page.

---
+++ tests/test-rotated-page.sh
@@ -0,0 +1,12 @@
+# Copyright © 2009 Jakub Wilk
+#
+# This package is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 dated June, 1991.
+
+# http://code.google.com/p/pdf2djvu/issues/detail?id=4
+# Fixed in [b58852fbb5ec].
+
+. common.sh
+
+test_pdf2djvu
