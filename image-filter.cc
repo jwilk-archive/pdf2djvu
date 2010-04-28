12f2c283451c28e4b5e3b68a922620db05376b4b
Jakub Wilk <jwilk@jwilk.net>
2010-04-28 18:50:33 +0200

    WIP

--- image-filter.cc
+++ image-filter.cc
@@ -21,123 +21,144 @@
 #include "djvuconst.hh"
 #include "rle.hh"
 
-static void dummy_quantizer(int width, int height, int *background_color, std::ostream &stream);
-
-void WebSafeQuantizer::output_web_palette(std::ostream &stream)
+void image::Bitmap::output_r4(std::ostream &stream)
 {
-  stream << "216" << std::endl;
-  for (int r = 0; r < 6; r++)
-  for (int g = 0; g < 6; g++)
-  for (int b = 0; b < 6; b++)
+  stream << "R4 " << this->width << " " << this->height << "\n";
+  for (unsigned int y = 0; y < this->height; y++)
   {
-    unsigned char buffer[] = { 51 * r, 51 * g, 51 * b };
-    stream.write(reinterpret_cast<char*>(buffer), 3);
-  }
-}
-
-static inline void write_uint32(std::ostream &stream, uint32_t item)
-{
-  unsigned char buffer[4];
-  for (int i = 0; i < 4; i++)
-    buffer[i] = item >> ((3 - i) * 8);
-  stream.write(reinterpret_cast<char*>(buffer), 4);
-}
-
-void MaskQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, int width, int height,
-  int *background_color, bool &has_foreground, bool &has_background, std::ostream &stream)
-{
-  if (out_fg == out_bg)
-  { /* Don't bother to analyze images if they are obviously identical. */
-    dummy_quantizer(width, height, background_color, stream);
-    has_background = true;
-    return;
-  }
-  rle::R4 r4(stream, width, height);
-  pdf::Pixmap bmp_fg(out_fg);
-  pdf::Pixmap bmp_bg(out_bg);
-  pdf::Pixmap::iterator p_fg = bmp_fg.begin();
-  pdf::Pixmap::iterator p_bg = bmp_bg.begin();
-  for (int y = 0; y < height; y++)
-  {
-    for (int x = 0; x < width; x++)
+    const std::vector<uint8_t> run = this->runs[y];
+    std::vector<uint8_t>::const_iterator it;
+    for (it = run.begin(); it != run.end(); it++)
     {
-      if (!has_background)
-      {
-        for (int i = 0; i < 3; i++)
-        if (background_color[i] != p_bg[i])
-        {
-          has_background = true;
-          break;
-        }
-      }
-      if (p_fg[0] != p_bg[0] || p_fg[1] != p_bg[1] || p_fg[2] != p_bg[2])
-      {
-        if (!has_foreground && (p_fg[0] || p_fg[1] || p_fg[2]))
-          has_foreground = true;
-        r4 << 1;
-      }
+      uint8_t length = *it;
+      if (length >= 192)
+        stream
+          << static_cast<char>(0xc0 + (length >> 8))
+          << static_cast<char>(length & 0xff);
       else
-        r4 << 0;
-      p_fg++, p_bg++;
+        stream << static_cast<char>(length);
     }
-    p_fg.next_row(), p_bg.next_row();
   }
-
 }
 
-void WebSafeQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, int width, int height,
-  int *background_color, bool &has_foreground, bool &has_background, std::ostream &stream)
+void image::Filter::filter(FilterData &filter_data)
 {
-  if (out_fg == out_bg)
-  { /* Don't bother to analyze images if they are obviously identical. */
-    dummy_quantizer(width, height, background_color, stream);
-    has_background = true;
-    return;
+  this->black_foreground = true;
+  this->solid_background = true;
+  this->width = filter_data.width;
+  this->height = filter_data.height;
+  this->sep_stream = &filter_data.sep_stream;
+  this->background_color[0] = this->background_color[1] = this->background_color[2] = 0xff;
+  { /* Extract image (foreground + background). */
+    this->image.reset();
+    this->extract_image(filter_data.r1);
   }
-  stream << "R6 " << width << " " << height << " ";
-  output_web_palette(stream);
-  pdf::Pixmap bmp_fg(out_fg);
-  pdf::Pixmap bmp_bg(out_bg);
-  pdf::Pixmap::iterator p_fg = bmp_fg.begin();
-  pdf::Pixmap::iterator p_bg = bmp_bg.begin();
-  for (int i = 0; i < 3; i++)
-    background_color[i] = p_bg[i];
-  for (int y = 0; y < height; y++)
-  {
-    int new_color, color = 0xfff;
-    int length = 0;
-    for (int x = 0; x < width; x++)
+  { /* Extract mask. */
+    bool ignorable = &filter_data.r1 == &filter_data.r2;
+    this->mask.reset();
+    this->extract_mask(filter_data.r2, ignorable);
+  }
+  this->process_image();
+}
+
+void image::Filter::extract_image(pdf::Renderer &r)
+{
+  this->image.reset(new pdf::Pixmap(&r));
+}
+
+void image::Filter::extract_mask(pdf::Renderer &r2, bool ignorable)
+{
+  this->black_foreground = true;
+  this->solid_background = true;
+  pdf::Pixmap::iterator it1 = this->image->begin();
+  if (ignorable)
+  { /* Optimize the case when mask is a priori known to be empty. */
+    for (unsigned int y = 0; y < height; y++)
     {
-      if (!has_background)
+      for (unsigned int x = 0; x < width; x++)
       {
-        for (int i = 0; i < 3; i++)
-        if (background_color[i] != p_bg[i])
+        if (this->solid_background)
         {
-          has_background = true;
+          for (unsigned int i = 0; i < 3; i++)
+          if (background_color[i] != it1[i])
+          {
+            this->solid_background = false;
+            return;
+          }
+        }
+        it1++;
+      }
+      it1.next_row();
+    }
+  }
+  pdf::Pixmap image2(&r2);
+  pdf::Pixmap::iterator it2 = image2.begin();
+  this->mask.reset(new image::Bitmap(this->width, this->height));
+  for (unsigned int y = 0; y < height; y++)
+  {
+    for (unsigned int x = 0; x < width; x++)
+    {
+      if (this->solid_background)
+      {
+        for (unsigned int i = 0; i < 3; i++)
+        if (background_color[i] != it2[i])
+        {
+          this->solid_background = false;
           break;
         }
       }
-      if (p_fg[0] != p_bg[0] || p_fg[1] != p_bg[1] || p_fg[2] != p_bg[2])
+      if (it1[0] != it2[0] || it1[1] != it2[1] || it1[2] != it2[2])
       {
-        if (!has_foreground && (p_fg[0] || p_fg[1] || p_fg[2]))
-          has_foreground = true;
-        new_color = ((p_fg[2] + 1) / 43) + 6 * (((p_fg[1] + 1) / 43) + 6 * ((p_fg[0] + 1) / 43));
+        if (!this->black_foreground && (it1[0] || it1[1] || it1[2]))
+          this->black_foreground = true;
+        *this->mask << 1;
       }
       else
-        new_color = 0xfff;
+        *this->mask << 0;
+      it1++, it2++;
+    }
+    it1.next_row(), it2.next_row();
+    this->mask->next_row();
+  }
+}
+
+void image::MaskFilter::process_image()
+{
+  this->mask->output_r4(*this->sep_stream);
+}
+
+void image::WebSafeFilter::process_image()
+{
+  rle::R6 r6(*this->sep_stream, width, height);
+  r6.use_web_palette();
+
+  pdf::Pixmap::iterator i_image = this->image->begin();
+  image::Bitmap::iterator i_mask = this->mask->begin();
+  for (unsigned int i = 0; i < 3; i++)
+    background_color[i] = i_image[i];
+  for (unsigned int y = 0; y < this->height; y++)
+  {
+    unsigned int new_color, color = 0xfff;
+    unsigned int length = 0;
+    for (unsigned int x = 0; x < this->width; x++)
+    {
+      new_color =
+        *i_mask
+        ? ((i_image[2] + 1) / 43) + 6 * (((i_image[1] + 1) / 43) + 6 * ((i_image[0] + 1) / 43))
+        : 0xfff;
       if (color == new_color)
         length++;
       else
       {
         if (length > 0)
-          write_uint32(stream, ((uint32_t)color << 20) + length);
+          r6.output_run(color, length);
         color = new_color;
         length = 1;
       }
-      p_fg++, p_bg++;
+      i_image++, i_mask++;
     }
-    p_fg.next_row(), p_bg.next_row();
-    write_uint32(stream, ((uint32_t)color << 20) + length);
+    i_image.next_row(), i_image.next_row();
+    r6.output_run(color, length);
   }
 }
 
@@ -199,7 +220,7 @@ class Run
 {
 protected:
   Rgb18 color;
-  size_t length;
+  unsigned int length;
 public:
   explicit Run(Rgb18 color)
   : color(color), length(0)
@@ -219,52 +240,30 @@ public:
   {
     return this->color;
   }
-  size_t get_length() const
+  unsigned int get_length() const
   {
     return this->length;
   }
 };
 
-void DefaultQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, int width, int height,
-  int *background_color, bool &has_foreground, bool &has_background, std::ostream &stream)
+void image::DefaultFilter::process_image()
 {
-  if (out_fg == out_bg)
-  { /* Don't bother to analyze images if they are obviously identical. */
-    dummy_quantizer(width, height, background_color, stream);
-    has_background = true;
-    return;
-  }
-  stream << "R6 " << width << " " << height << " ";
-  pdf::Pixmap bmp_fg(out_fg);
-  pdf::Pixmap bmp_bg(out_bg);
-  pdf::Pixmap::iterator p_fg = bmp_fg.begin();
-  pdf::Pixmap::iterator p_bg = bmp_bg.begin();
-  size_t color_counter = 0;
+  rle::R6 r6(*this->sep_stream, width, height);
+  pdf::Pixmap::iterator i_image = this->image->begin();
+  image::Bitmap::iterator i_mask = this->mask->begin();
+  unsigned int color_counter = 0;
   std::bitset<1 << 18> original_colors;
   std::bitset<1 << 18> quantized_colors;
   std::vector<std::vector<Run> > runs(height);
-  for (int i = 0; i < 3; i++)
-    background_color[i] = p_bg[i];
-  for (int y = 0; y < height; y++)
+  for (unsigned int y = 0; y < height; y++)
   {
     Run run;
     Rgb18 new_color;
-    for (int x = 0; x < width; x++)
+    for (unsigned int x = 0; x < width; x++)
     {
-      if (!has_background)
+      if (*i_mask)
       {
-        for (int i = 0; i < 3; i++)
-        if (background_color[i] != p_bg[i])
-        {
-          has_background = true;
-          break;
-        }
-      }
-      if (p_fg[0] != p_bg[0] || p_fg[1] != p_bg[1] || p_fg[2] != p_bg[2])
-      {
-        if (!has_foreground && (p_fg[0] || p_fg[1] || p_fg[2]))
-          has_foreground = true;
-        new_color = Rgb18(p_fg[0], p_fg[1], p_fg[2]);
+        new_color = Rgb18(i_image[0], i_image[1], i_image[2]);
         if (!original_colors[new_color])
         {
           color_counter++;
@@ -282,9 +281,9 @@ void DefaultQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, 
         run = Run(new_color);
         run++;
       }
-      p_fg++, p_bg++;
+      i_image++, i_mask++;
     }
-    p_fg.next_row(), p_bg.next_row();
+    i_image.next_row(), i_mask.next_row();
     if (run.get_length() > 0)
       runs[y].push_back(run);
   }
@@ -292,7 +291,7 @@ void DefaultQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, 
   int divisor = 4;
   while (color_counter > djvu::max_fg_colors)
   {
-    size_t new_color_counter = 0;
+    unsigned int new_color_counter = 0;
     quantized_colors.reset();
     divisor++;
     for (size_t color = 0; color < original_colors.size(); color++)
@@ -314,27 +313,20 @@ void DefaultQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, 
     quantized_colors = original_colors;
   /* Output the palette: */
   if (color_counter == 0)
-  {
-    stream << 1 << std::endl << "\xff\xff\xff";
-  }
+    r6.use_dummy_palette();
   else
   {
-    stream << color_counter << std::endl;
+    r6.use_palette(color_counter);
     for (size_t color = 0; color < quantized_colors.size(); color++)
-    {
       if (quantized_colors[color])
       {
         Rgb18 rgb18(color);
-        unsigned char buffer[3];
-        for (int i = 0; i < 3; i++)
-          buffer[i] = rgb18[i];
-        stream.write(reinterpret_cast<char*>(buffer), 3);
+        r6.add_color(rgb18[0], rgb18[1], rgb18[2]);
       }
-    }
   }
   /* Map colors into color indices: */
-  std::map<int, uint32_t> color_map;
-  uint32_t last_color_index = 0;
+  std::map<int, unsigned int> color_map;
+  int last_color_index = 0;
   color_map[-1] = 0xfff;
   if (divisor == 4)
     for (size_t color = 0; color < original_colors.size(); color++)
@@ -345,7 +337,7 @@ void DefaultQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, 
     }
   else
   {
-    std::map<int, uint32_t> quantized_color_map;
+    std::map<int, unsigned int> quantized_color_map;
     for (size_t color = 0; color < quantized_colors.size(); color++)
     {
       if (!quantized_colors[color])
@@ -359,81 +351,59 @@ void DefaultQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, 
     }
   }
   /* Output runs: */
-  for (int y = 0; y < height; y++)
+  for (unsigned int y = 0; y < height; y++)
   {
     const std::vector<Run> line_runs = runs[y];
     for (std::vector<Run>::const_iterator run = line_runs.begin(); run != line_runs.end(); run++)
     {
-      uint32_t color_index = color_map[run->get_color()];
-      write_uint32(stream, ((uint32_t)color_index << 20) + run->get_length());
+      unsigned int color_index = color_map[run->get_color()];
+      r6.output_run(color_index, run->get_length());
     }
   }
 }
 
-static void dummy_quantizer(int width, int height, int *background_color, std::ostream &stream)
+void image::DummyFilter::extract_image(pdf::Renderer &)
 {
-  rle::R4 r4(stream, width, height);
-  for (int y = 0; y < height; y++)
-    r4.output_run(width);
-  background_color[0] = background_color[1] = background_color[2] = 0xff;
+  this->image.reset(NULL);
 }
 
-void DummyQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, int width, int height,
-  int *background_color, bool &has_foreground, bool &has_background, std::ostream &stream)
+void image::DummyFilter::extract_mask(pdf::Renderer &, bool ignorable)
 {
-  dummy_quantizer(width, height, background_color, stream);
+  this->mask.reset(NULL);
+}
+
+void image::DummyFilter::process_image()
+{
+  rle::R4 r4(*this->sep_stream, width, height);
+  for (unsigned int y = 0; y < height; y++)
+    r4.output_run(width);
+  this->background_color[0] = this->background_color[1] = this->background_color[2] = 0xff;
 }
 
 #if HAVE_GRAPHICSMAGICK
 
-GraphicsMagickQuantizer::GraphicsMagickQuantizer(const Config &config)
-: Quantizer(config)
-{ }
-
-void GraphicsMagickQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, int width, int height,
-  int *background_color, bool &has_foreground, bool &has_background, std::ostream &stream)
+void image::GraphicsMagickFilter::process_image()
 {
-  if (out_fg == out_bg)
-  { /* Don't bother to analyze images if they are obviously identical. */
-    dummy_quantizer(width, height, background_color, stream);
-    has_background = true;
-    return;
-  }
-  stream << "R6 " << width << " " << height << " ";
+  rle::R6 r6(*this->sep_stream, width, height);
   Magick::Image image(Magick::Geometry(width, height), Magick::Color());
   image.type(Magick::TrueColorMatteType);
   image.modifyImage();
-  pdf::Pixmap bmp_fg(out_fg);
-  pdf::Pixmap bmp_bg(out_bg);
-  pdf::Pixmap::iterator p_fg = bmp_fg.begin();
-  pdf::Pixmap::iterator p_bg = bmp_bg.begin();
+  pdf::Pixmap::iterator i_image = this->image->begin();
+  image::Bitmap::iterator i_mask = this->mask->begin();
   for (int i = 0; i < 3; i++)
-    background_color[i] = p_bg[i];
-  for (int y = 0; y < height; y++)
+    background_color[i] = i_image[i];
+  for (unsigned int y = 0; y < height; y++)
   {
     Magick::PixelPacket* ipixel = image.setPixels(0, y, width, 1);
-    for (int x = 0; x < width; x++)
+    for (unsigned int x = 0; x < width; x++)
     {
-      if (!has_background)
-      {
-        for (int i = 0; i < 3; i++)
-        if (background_color[i] != p_bg[i])
-        {
-          has_background = true;
-          break;
-        }
-      }
-      if (p_fg[0] != p_bg[0] || p_fg[1] != p_bg[1] || p_fg[2] != p_bg[2])
-      {
-        if (!has_foreground && (p_fg[0] || p_fg[1] || p_fg[2]))
-          has_foreground = true;
-        *ipixel = Magick::Color(p_fg[0], p_fg[1], p_fg[2], 0);
-      }
-      else
-        *ipixel = Magick::Color(0, 0, 0, 0xff);
-      p_fg++, p_bg++, ipixel++;
+      *ipixel =
+        *i_mask
+        ? Magick::Color(i_image[0], i_image[1], i_image[2], 0)
+        : Magick::Color(0, 0, 0, 0xff);
+      i_image++, i_mask++;
     }
-    p_fg.next_row(), p_bg.next_row(), image.syncPixels();
+    i_image++, i_mask++, image.syncPixels();
   }
   image.quantizeColorSpace(Magick::TransparentColorspace);
   image.quantizeColors(this->config.fg_colors);
@@ -443,20 +413,19 @@ void GraphicsMagickQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *o
   image.quantizeColors(9999);
   image.quantize();
   unsigned int n_colors = image.colorMapSize();
-  stream << n_colors << std::endl;
+  r6.use_palette(n_colors);
   for (unsigned int i = 0; i < n_colors; i++)
   {
     const Magick::Color &color = image.colorMap(i);
-    char buffer[] = { color.redQuantum(), color.greenQuantum(), color.blueQuantum() };
-    stream.write(buffer, 3);
+    r6.add_color(color.redQuantum(), color.greenQuantum(), color.blueQuantum());
   }
-  for (int y = 0; y < height; y++)
+  for (unsigned int y = 0; y < height; y++)
   {
     int new_color, color = 0xfff;
     Magick::PixelPacket *ipixel = image.getPixels(0, y, width, 1);
     Magick::IndexPacket *ppixel = image.getIndexes();
     int length = 0;
-    for (int x = 0; x < width; x++)
+    for (unsigned int x = 0; x < width; x++)
     {
       if (ipixel->opacity != TransparentOpacity)
         new_color = *ppixel;
@@ -467,27 +436,22 @@ void GraphicsMagickQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *o
       else
       {
         if (length > 0)
-          write_uint32(stream, ((uint32_t)color << 20) + length);
+          r6.output_run(color, length);
         color = new_color;
         length = 1;
       }
       ipixel++, ppixel++;
     }
-    write_uint32(stream, ((uint32_t)color << 20) + length);
+    r6.output_run(color, length);
   }
 }
 
 #else
 
-GraphicsMagickQuantizer::GraphicsMagickQuantizer(const Config &config)
-: Quantizer(config)
+image::GraphicsMagickFilter::process_image()
 {
   throw NotImplementedError();
 }
-
-void GraphicsMagickQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, int width, int height,
-  int *background_color, bool &has_foreground, bool &has_background, std::ostream &stream)
-{ /* just to satisfy compilers */ }
 
 #endif
 
--- image-filter.hh
+++ image-filter.hh
@@ -1,4 +1,4 @@
-/* Copyright © 2008, 2009 Jakub Wilk
+/* Copyright © 2008, 2009, 2010 Jakub Wilk
  * Copyright © 2009 Mateusz Turcza
  *
  * This package is free software; you can redistribute it and/or modify
@@ -9,80 +9,195 @@
 #ifndef PDF2DJVU_IMAGE_FILTER_H
 #define PDF2DJVU_IMAGE_FILTER_H
 
+#include <cassert>
 #include <stdexcept>
 
 #include "pdf-backend.hh"
 #include "config.hh"
 #include "i18n.hh"
 
-class Quantizer
-{
-protected:
-  const Config &config;
-public:
-  virtual void operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, int width, int height,
-    int *background_color, bool &has_foreground, bool &has_background, std::ostream &stream) = 0;
-  explicit Quantizer(const Config &config) : config(config) { }
-  virtual ~Quantizer() throw ()
-  { }
-};
+namespace image {
 
-class DefaultQuantizer : public Quantizer
-{
-public:
-  explicit DefaultQuantizer(const Config &config)
-  : Quantizer(config)
-  { }
-  virtual void operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, int width, int height,
-    int *background_color, bool &has_foreground, bool &has_background, std::ostream &stream);
-};
+  class BitmapIterator;
 
-class WebSafeQuantizer : public Quantizer
-{
-protected:
-  void output_web_palette(std::ostream &stream);
-public:
-  explicit WebSafeQuantizer(const Config &config)
-  : Quantizer(config)
-  { }
-  virtual void operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, int width, int height,
-    int *background_color, bool &has_foreground, bool &has_background, std::ostream &stream);
-};
-
-class MaskQuantizer : public Quantizer
-{
-public:
-  explicit MaskQuantizer(const Config &config)
-  : Quantizer(config)
-  { }
-  virtual void operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, int width, int height,
-    int *background_color, bool &has_foreground, bool &has_background, std::ostream &stream);
-};
-
-class DummyQuantizer : public Quantizer
-{
-public:
-  explicit DummyQuantizer(const Config &config)
-  : Quantizer(config)
-  { }
-  virtual void operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, int width, int height,
-    int *background_color, bool &has_foreground, bool &has_background, std::ostream &stream);
-};
-
-class GraphicsMagickQuantizer : public Quantizer
-{
-public:
-  explicit GraphicsMagickQuantizer(const Config &config);
-  virtual void operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, int width, int height,
-    int *background_color, bool &has_foreground, bool &has_background, std::ostream &stream);
-  class NotImplementedError : public std::runtime_error
+  class Bitmap
   {
+  protected:
+    std::vector< std::vector<uint8_t> > runs;
+    unsigned int width, height;
+    unsigned int x, y;
+    bool last_pixel;
   public:
-    NotImplementedError()
-    : std::runtime_error(_("Advanced color quantization is not supported."))
-    { };
+    Bitmap(unsigned int width, unsigned int height)
+    : width(width), height(height),
+      x(0), y(0),
+      last_pixel(0)
+    {
+      runs.resize(height);
+    }
+    void next_row()
+    {
+      this->y++;
+      this->x = 0;
+      this->last_pixel = 0;
+      this->runs[this->y].push_back(0);
+    }
+    void operator <<(bool pixel)
+    {
+      assert(this->x < this->width);
+      assert(this->y < this->height);
+      this->x++;
+      if (pixel != this->last_pixel)
+        this->runs[this->y].push_back(1);
+      else if (this->last_pixel == pixel)
+      {
+        if (runs[this->y].back()++ == 0)
+        {
+          this->runs[this->y].back() = 0xff;
+          this->runs[this->y].push_back(0);
+          this->runs[this->y].push_back(1);
+        }
+      }
+      this->last_pixel = pixel;
+    }
+    void output_r4(std::ostream &);
+    void output_pbm(std::ostream &);
+    typedef BitmapIterator iterator;
+    friend class BitmapIterator;
+    iterator & begin() const;
   };
-};
+
+  class BitmapIterator
+  {
+  protected:
+    const Bitmap &bitmap;
+    unsigned int y, u, n;
+  public:
+    explicit BitmapIterator(const Bitmap &)
+    : bitmap(bitmap),
+      y(0), u(0), n(0)
+    { }
+    bool operator *()
+    {
+      return this->u & 1;
+    }
+    void operator ++(int)
+    {
+      this->n++;
+      while (this->bitmap.runs[this->y][this->u] >= this->n)
+      {
+        this->u++;
+        this->n = 0;
+      }
+    }
+    void next_row()
+    {
+      this->y++;
+      this->u = this-> n = 0;
+    }
+  };
+
+  class FilterData
+  {
+  protected:
+    const Config &config;
+    pdf::Renderer &r1;
+    pdf::Renderer &r2;
+    unsigned int width, height;
+    std::ostream &sep_stream;
+  protected:
+    explicit FilterData(
+      const Config &config,
+      pdf::Renderer &r1, pdf::Renderer &r2,
+      unsigned int width, unsigned int height,
+      std::ostream &sep_stream
+    )
+    : config(config),
+      r1(r1), r2(r2),
+      width(width), height(height),
+      sep_stream(sep_stream)
+    { }
+    friend class Filter;
+  };
+
+  class Filter
+  {
+  protected:
+    const Config &config;
+    unsigned int width, height;
+    bool black_foreground;
+    bool solid_background;
+    uint8_t background_color[3];
+    std::ostream *sep_stream;
+    std::auto_ptr<image::Bitmap> mask;
+    std::auto_ptr<pdf::Pixmap> image;
+    pdf::Renderer *r1, *r2;
+    explicit Filter(const Config &config)
+    : config(config)
+    { }
+  protected:
+    virtual void extract_image(pdf::Renderer &);
+    virtual void extract_mask(pdf::Renderer &, bool ignorable);
+    virtual void process_image() = 0;
+  public:
+    virtual void filter(FilterData &);
+    pdf::Pixmap * get_image()
+    {
+      return this->image.get();
+    };
+    image::Bitmap * get_mask()
+    {
+      return this->mask.get();
+    }
+    bool has_black_foreground() const
+    {
+      return this->black_foreground;
+    }
+    bool has_solid_background() const
+    {
+      return this->solid_background;
+    }
+    uint8_t *get_background_color()
+    {
+      return this->background_color;
+    }
+  };
+
+  class DefaultFilter : public Filter
+  {
+    virtual void process_image();
+  };
+
+  class WebSafeFilter : public Filter
+  {
+    virtual void process_image();
+  };
+
+  class MaskFilter : public Filter
+  {
+    virtual void process_image();
+  };
+
+  class DummyFilter : public Filter
+  {
+    virtual void extract_image(pdf::Renderer &);
+    virtual void extract_mask(pdf::Renderer &, bool ignorable);
+    virtual void process_image();
+  };
+
+  class GraphicsMagickFilter : public Filter
+  {
+    virtual void process_image();
+    class NotImplementedError : public std::runtime_error
+    {
+    public:
+      NotImplementedError()
+      : std::runtime_error(_("Advanced color quantization is not supported."))
+      { };
+    };
+  };
+
+}
 
 #endif
 
