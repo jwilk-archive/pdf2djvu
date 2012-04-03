cab696c9380f9c0adc7feedfdc5769883d85d27d
Jakub Wilk <jwilk@jwilk.net>
2012-04-03 11:32:07 +0200

    tests: expand tabs.

--- tests/common.py
+++ tests/common.py
@@ -11,15 +11,15 @@ import Image
 import ImageColor
 
 def rainbow(width, height):
-	image = Image.new('RGB', (width, height))
+    image = Image.new('RGB', (width, height))
 
-	pixels = image.load()
-	for x in xrange(width):
-		for y in xrange(height):
-			hue = 255 * x // (width - 1)
-			luminance = 100 * y // height
-			color = ImageColor.getrgb('hsl(%(hue)d, 100%%, %(luminance)d%%)' % locals())
-			pixels[x, y] = color
-	return image
+    pixels = image.load()
+    for x in xrange(width):
+        for y in xrange(height):
+            hue = 255 * x // (width - 1)
+            luminance = 100 * y // height
+            color = ImageColor.getrgb('hsl(%(hue)d, 100%%, %(luminance)d%%)' % locals())
+            pixels[x, y] = color
+    return image
 
-# vim:ts=4 sw=4 noet
+# vim:ts=4 sw=4 et
