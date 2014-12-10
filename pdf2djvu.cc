f9a7cc599dfdbaeb148d4416f2aceeb8a81b15e0
Jakub Wilk <jwilk@jwilk.net>
2014-12-10 22:41:52 +0100

    Back out interpolation disabling code. It turns out not to be very useful: https://bugs.debian.org/760396#30

--- pdf2djvu.cc
+++ pdf2djvu.cc
@@ -294,50 +294,7 @@ public:
   }
 };
 
-class MainRenderer: public pdf::Renderer
-{
-public:
-  MainRenderer(pdf::splash::Color &paper_color, bool monochrome)
-  : Renderer(paper_color, monochrome)
-  { }
-
-  /* Disable image interpolation, which has detrimental effect on compression ratio.
-   * https://bugs.debian.org/760396
-   */
-
-  void drawImageMask(pdf::gfx::State *state, pdf::Object *object, pdf::Stream *stream, int width, int height,
-    pdf::Bool invert, pdf::Bool interpolate, pdf::Bool inline_image)
-  {
-    pdf::Renderer::drawImageMask(state, object, stream, width, height,
-        invert, /* interpolate */ 0, inline_image);
-  }
-
-  void drawImage(pdf::gfx::State *state, pdf::Object *object, pdf::Stream *stream, int width, int height,
-    pdf::gfx::ImageColorMap *color_map, pdf::Bool interpolate, int *mask_colors, pdf::Bool inline_image)
-  {
-    pdf::Renderer::drawImage(state, object, stream, width, height,
-        color_map, /* interpolate */ 0, mask_colors, inline_image);
-  }
-
-  void drawMaskedImage(pdf::gfx::State *state, pdf::Object *object, pdf::Stream *stream, int width, int height,
-    pdf::gfx::ImageColorMap *color_map, pdf::Bool interpolate,
-    pdf::Stream *mask_stream, int mask_width, int mask_height, pdf::Bool mask_invert, pdf::Bool mask_interpolate)
-  {
-    pdf::Renderer::Renderer::drawMaskedImage(state, object, stream, width, height,
-      color_map, /* interpolate */ 0,
-      mask_stream, mask_width, mask_height, mask_invert, /* mask_interpolate */ 0);
-  }
-
-  void drawSoftMaskedImage(pdf::gfx::State *state, pdf::Object *object, pdf::Stream *stream,
-    int width, int height, pdf::gfx::ImageColorMap *color_map, pdf::Bool interpolate,
-    pdf::Stream *mask_stream, int mask_width, int mask_height,
-    pdf::gfx::ImageColorMap *mask_color_map, pdf::Bool mask_interpolate)
-  {
-    pdf::Renderer::drawSoftMaskedImage(state, object, stream, width, height,
-      color_map, /* interpolate */ 0,
-      mask_stream, mask_width, mask_height, mask_color_map, /* mask_interpolate */ 0);
-  }
-};
+typedef pdf::Renderer MainRenderer;
 
 class MutedRenderer: public pdf::Renderer
 {
@@ -393,7 +350,7 @@ public:
       return;
     }
     Renderer::drawImage(state, object, stream, width, height, color_map,
-        /* interpolate */ 0, mask_colors, inline_image);
+        interpolate, mask_colors, inline_image);
   }
 
   void drawMaskedImage(pdf::gfx::State *state, pdf::Object *object, pdf::Stream *stream, int width, int height,
@@ -406,8 +363,8 @@ public:
       return;
     }
     Renderer::drawMaskedImage(state, object, stream, width, height,
-      color_map, /* interpolate */ 0,
-      mask_stream, mask_width, mask_height, mask_invert, /* mask_interpolate */ 0);
+      color_map, interpolate,
+      mask_stream, mask_width, mask_height, mask_invert, mask_interpolate);
   }
 
   void drawSoftMaskedImage(pdf::gfx::State *state, pdf::Object *object, pdf::Stream *stream,
@@ -421,8 +378,8 @@ public:
       return;
     }
     Renderer::drawSoftMaskedImage(state, object, stream, width, height,
-      color_map, /* interpolate */ 0,
-      mask_stream, mask_width, mask_height, mask_color_map, /* mask_interpolate */ 0);
+      color_map, interpolate,
+      mask_stream, mask_width, mask_height, mask_color_map, mask_interpolate);
   }
 
   pdf::Bool interpretType3Chars() { return false; }
