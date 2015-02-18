a043954ac3f926e46216960ef3e925adecbc480e
Jakub Wilk <jwilk@jwilk.net>
2015-02-18 21:33:00 +0100

    tests: update copyright information.

--- tests/test-no-meta.py
+++ tests/test-no-meta.py
@@ -1,6 +1,6 @@
 # encoding=UTF-8
 
-# Copyright © 2009, 2011, 2012 Jakub Wilk
+# Copyright © 2009-2012 Jakub Wilk
 #
 # This package is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
