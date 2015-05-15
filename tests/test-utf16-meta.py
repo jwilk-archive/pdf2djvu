695d52220f8393c870a7c4b486803a0e974cdc93
Jakub Wilk <jwilk@jwilk.net>
2015-05-15 19:57:47 +0200

    tests/test-utf16-meta: wrap a very long line.

--- tests/test-utf16-meta.py
+++ tests/test-utf16-meta.py
@@ -1,6 +1,6 @@
 # encoding=UTF-8
 
-# Copyright © 2009-2012 Jakub Wilk
+# Copyright © 2009-2015 Jakub Wilk
 #
 # This package is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,10 @@ class test(case):
     fixed in [90111a528eb5]
     '''
     def test(self):
-        title = r'\316\232\317\211\316\264\316\271\316\272\316\277\317\200\316\277\316\257\316\267\317\203\316\267 \317\207\316\261\317\201\316\261\316\272\317\204\316\256\317\201\317\211\316\275'
+        title = (
+            r'\316\232\317\211\316\264\316\271\316\272\316\277\317\200\316\277\316\257\316\267\317\203\316\267 '
+            r'\317\207\316\261\317\201\316\261\316\272\317\204\316\256\317\201\317\211\316\275'
+        )
         self.pdf2djvu('--dpi=72').assert_()
         r = self.print_meta()
         r.assert_(stdout=re('Title\t"%s"' % title.replace('\\', '\\\\')))
