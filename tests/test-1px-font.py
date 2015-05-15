f5c7483a5630a6618194b65f380f1a484994e74f
Jakub Wilk <jwilk@jwilk.net>
2015-05-15 19:51:34 +0200

    tests/test-1px-font: add a trailing comma to the import list.

--- tests/test-1px-font.py
+++ tests/test-1px-font.py
@@ -1,13 +1,13 @@
 # encoding=UTF-8
 
-# Copyright © 2009-2014 Jakub Wilk
+# Copyright © 2009-2015 Jakub Wilk
 #
 # This package is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; version 2 dated June, 1991.
 
 from common import (
-    case
+    case,
 )
 
 class test(case):
