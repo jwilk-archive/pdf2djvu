2d186ddca39bf593cb506405262798c90fbd5018
Jakub Wilk <jwilk@jwilk.net>
2015-08-05 13:03:01 +0200

    sexpr: increase indentation.

--- sexpr.cc
+++ sexpr.cc
@@ -16,22 +16,22 @@
 
 static int my_puts(miniexp_io_t* io, const char *s)
 {
-  std::ostream *puts_stream = reinterpret_cast<std::ostream*>(io->data[0]);
-  *puts_stream << s;
-  return puts_stream->good() ? 0 : EOF;
+    std::ostream *puts_stream = reinterpret_cast<std::ostream*>(io->data[0]);
+    *puts_stream << s;
+    return puts_stream->good() ? 0 : EOF;
 }
 
 namespace sexpr
 {
-  std::ostream &operator<<(std::ostream &stream, const sexpr::Ref &expr)
-  {
-    miniexp_io_t io;
-    miniexp_io_init(&io);
-    io.fputs = my_puts;
-    io.data[0] = &stream;
-    miniexp_prin_r(&io, expr);
-    return stream;
-  }
+    std::ostream &operator<<(std::ostream &stream, const sexpr::Ref &expr)
+    {
+        miniexp_io_t io;
+        miniexp_io_init(&io);
+        io.fputs = my_puts;
+        io.data[0] = &stream;
+        miniexp_prin_r(&io, expr);
+        return stream;
+    }
 }
 
 #else
@@ -40,23 +40,23 @@ static std::ostream *puts_stream = NULL;
 
 static int my_puts(const char *s)
 {
-  *puts_stream << s;
-  return puts_stream->good() ? 0 : EOF;
+    *puts_stream << s;
+    return puts_stream->good() ? 0 : EOF;
 }
 
 namespace sexpr
 {
-  std::ostream &operator<<(std::ostream &stream, const sexpr::Ref &expr)
-  {
-    int (*old_minilisp_puts)(const char *s) = minilisp_puts;
-    minilisp_puts = my_puts;
-    puts_stream = &stream;
-    miniexp_prin(expr);
-    minilisp_puts = old_minilisp_puts;
-    return stream;
-  }
+    std::ostream &operator<<(std::ostream &stream, const sexpr::Ref &expr)
+    {
+        int (*old_minilisp_puts)(const char *s) = minilisp_puts;
+        minilisp_puts = my_puts;
+        puts_stream = &stream;
+        miniexp_prin(expr);
+        minilisp_puts = old_minilisp_puts;
+        return stream;
+    }
 }
 
 #endif
 
-// vim:ts=2 sts=2 sw=2 et
+// vim:ts=4 sts=4 sw=4 et
