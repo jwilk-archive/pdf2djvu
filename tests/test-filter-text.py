cf5290096a9f797d5465d517d30fd33a5a87d2d3
Jakub Wilk <jwilk@jwilk.net>
2015-08-26 16:53:51 +0200

    tests/test-filter-text: add.

---
+++ tests/test-filter-text.py
@@ -0,0 +1,28 @@
+# encoding=UTF-8
+
+# Copyright © 2015 Jakub Wilk <jwilk@jwilk.net>
+#
+# This file is part of pdfdjvu.
+#
+# pdf2djvu is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# pdf2djvu is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+import re
+
+from tools import (
+    case,
+)
+
+class test(case):
+    def test_rot13(self):
+        self.pdf2djvu('--filter-text', 'tr "a-zA-Z" "n-za-mN-ZA-M"').assert_()
+        r = self.print_text()
+        r.assert_(stdout=re.compile('^Yberz vcfhz *\n'))
+
+# vim:ts=4 sts=4 sw=4 et
