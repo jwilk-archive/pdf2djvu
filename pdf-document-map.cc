75d4c1655a6502e5f14b726587e1fae829e36d6e
Jakub Wilk <jwilk@jwilk.net>
2015-09-09 10:15:54 +0200

    pdf-document-map: strip trailing \0 from labels only in old version of Poppler.

--- pdf-document-map.cc
+++ pdf-document-map.cc
@@ -29,13 +29,14 @@ pdf::DocumentMap::DocumentMap(const std::vector<const char *> &paths)
                 pdf::String s;
                 if (catalog->indexToLabel(i, &s)) {
                     std::string str = pdf::string_as_utf8(&s);
-                    if (str.length() > 0 && str.end()[-1] == '\0') {
-                        // Prior to 0.19.3, Poppler incorrectly adds trailing
-                        // NULL character to Unicode labels:
-                        // http://cgit.freedesktop.org/poppler/poppler/commit/?id=cef6ac0ebbf8
-                        // Let's strip it.
+#if POPPLER_VERSION < 1903
+                    // Prior to 0.19.3, Poppler incorrectly adds trailing
+                    // NULL character to Unicode labels:
+                    // http://cgit.freedesktop.org/poppler/poppler/commit/?id=cef6ac0ebbf8
+                    // Let's strip it.
+                    if (str.length() > 0 && str.end()[-1] == '\0')
                         str.erase(str.end() - 1);
-                    }
+#endif
                     this->labels.push_back(str);
                 }
                 else
