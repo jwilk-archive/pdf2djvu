9c0b20ac35efef24f13b9fbd3c7674f732ac9eec
Jakub Wilk <jwilk@jwilk.net>
2016-04-08 13:06:04 +0200

    Update copyright information.

--- system.cc
+++ system.cc
@@ -1,4 +1,4 @@
-/* Copyright © 2007-2015 Jakub Wilk <jwilk@jwilk.net>
+/* Copyright © 2007-2016 Jakub Wilk <jwilk@jwilk.net>
  * Copyright © 2009 Mateusz Turcza
  *
  * This file is part of pdf2djvu.
