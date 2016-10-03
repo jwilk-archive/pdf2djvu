f3f0d4e56909ad19d60b9a52ee300a5aaebab52a
Jakub Wilk <jwilk@jwilk.net>
2016-10-03 20:16:36 +0200

    tests/test-oversized-outline: add.

---
+++ tests/test-oversized-outline.py
@@ -0,0 +1,27 @@
+# encoding=UTF-8
+
+# Copyright © 2016 Jakub Wilk <jwilk@jwilk.net>
+#
+# This file is part of pdf2djvu.
+#
+# pdf2djvu is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# pdf2djvu is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+from tools import (
+    case,
+    re,
+)
+
+class test(case):
+
+    def test(self):
+        r = self.pdf2djvu()
+        r.assert_(stderr=re('Document outline too large\n'), rc=1)
+
+# vim:ts=4 sts=4 sw=4 et
