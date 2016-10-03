afd26f6e560892c9f5d4b2193298a75cf73c41e4
Jakub Wilk <jwilk@jwilk.net>
2016-10-03 21:13:16 +0200

    tests/test-big-outline: add.

---
+++ tests/test-big-outline.py
@@ -0,0 +1,41 @@
+# encoding=UTF-8
+
+# Copyright © 2016 Jakub Wilk <jwilk@jwilk.net>
+#
+# This file is part of pdf2djvu.
+#
+# pdf2djvu is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# pdf2djvu is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+from tools import (
+    case,
+    re,
+)
+
+b = 255
+item_template = '''
+  ("ipsum {0}"
+   "#p0001.djvu" )
+'''.strip('\n')
+expected_outline_sexpr = ('''\
+(bookmarks
+ ("Lorem"
+  "#p0001.djvu"
+'''
++ '\n'.join(item_template.format(i) for i in range(1, b + 1))
++ ' ) )\n'
+)
+
+class test(case):
+
+    def test_multi_page(self):
+        self.pdf2djvu().assert_()
+        self.print_outline().assert_(stdout=expected_outline_sexpr)
+
+# vim:ts=4 sts=4 sw=4 et
