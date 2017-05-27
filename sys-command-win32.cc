jwilk-archive/pdf2djvu

commit 0f36426294ffa136000fabb1dd1973eda61ff7f4

Modernize for loops.

sys-command-win32.cc
@@ -29,8 +29,8 @@ Command::Command(const std::string& command) : command(command)
 {
     // Convert path separators:
     std::ostringstream stream;
-    for (std::string::const_iterator it = command.begin(); it != command.end(); it++) {
-        if (*it == '/')
+    for (char c : command) {
+        if (c == '/')
             stream << '\\';
         else
             stream << *it;
@@ -83,15 +83,15 @@ static const std::string argv_to_command_line(const std::vector<std::string> &ar
     //    double quotation mark as described in rule 3.
     //
     // See <https://msdn.microsoft.com/en-us/library/ms880421.aspx>.
-    for (std::vector<std::string>::const_iterator parg = argv.begin(); parg != argv.end(); parg++) {
+    for (const std::string &arg : argv) {
         int backslashed = 0;
-        bool need_quote = parg->find_first_of(" \t") != std::string::npos;
+        bool need_quote = arg.find_first_of(" \t") != std::string::npos;
         if (need_quote)
             buffer << '"';
-        for (std::string::const_iterator pch = parg->begin(); pch != parg->end(); pch++) {
-            if (*pch == '\\')
+        for (char c : arg) {
+            if (c == '\\')
                 backslashed++;
-            else if (*pch == '"') {
+            else if (c == '"') {
                 for (int i = 0; i < backslashed; i++)
                     buffer << "\\\\";
                 backslashed = 0;
@@ -100,7 +100,7 @@ static const std::string argv_to_command_line(const std::vector<std::string> &ar
                 for (int i = 0; i < backslashed; i++)
                     buffer << '\\';
                 backslashed = 0;
-                buffer << *pch;
+                buffer << c;
             }
         }
         for (int i = 0; i < backslashed; i++)

config.cc
@@ -75,26 +75,26 @@ static void parse_hyperlinks_options(std::string s, Config::Hyperlinks &options)
   std::vector<std::string> split;
   std::replace(s.begin(), s.end(), '_', '-');
   string::split(s, ',', split);
-  for (std::vector<std::string>::const_iterator it = split.begin(); it != split.end(); it++)
+  for (const std::string &s : split)
   {
-    if (*it == "border-avis")
+    if (s == "border-avis")
     {
       options.border_always_visible = true;
       continue;
     }
-    else if (*it == "no" || *it == "none")
+    else if (s == "no" || s == "none")
     {
       options.extract = false;
       continue;
     }
     else if
     (
-      it->length() == 7 &&
-      (*it)[0] == '#' &&
-      it->find_first_not_of("0123456789abcdefABCDEF", 1) == std::string::npos
+      s.length() == 7 &&
+      s[0] == '#' &&
+      s.find_first_not_of("0123456789abcdefABCDEF", 1) == std::string::npos
     )
     {
-      options.border_color = *it;
+      options.border_color = s;
       continue;
     }
     throw Config::Error(_("Unable to parse hyperlinks options"));
@@ -110,20 +110,20 @@ static void parse_pages(const std::string &s, std::vector< std::pair<int, int> >
 {
   int state = 0;
   int value[2] = { 0, 0 };
-  for (std::string::const_iterator it = s.begin(); it != s.end(); it++)
+  for (char c : s)
   {
-    if (('0' <= *it) && (*it <= '9'))
+    if ('0' <= c && c <= '9')
     {
-      value[state] = value[state] * 10 + (int)(*it - '0');
+      value[state] = value[state] * 10 + (int)(c - '0');
       if (state == 0)
         value[1] = value[0];
     }
-    else if (state == 0 && *it == '-')
+    else if (state == 0 && c == '-')
     {
       state = 1;
       value[1] = 0;
     }
-    else if (*it == ',')
+    else if (c == ',')
     {
       if (value[0] < 1 || value[1] < 1 || value[0] > value[1])
         bad_pages();
@@ -208,19 +208,19 @@ static void validate_page_id_template(string_format::Template &page_id_template)
   size_t length = page_id.length();
   bool dot_allowed = false;
   bool pm_allowed = false;
-  for (std::string::const_iterator it = page_id.begin(); it != page_id.end(); it++)
+  for (char c : page_id)
   {
-    if (!pm_allowed && (*it == '+' || *it == '-'))
+    if (!pm_allowed && (c == '+' || c == '-'))
     {
       throw Config::Error(_("Page identifier cannot start with a '+' or a '-' character"));
     }
-    if (*it == '.')
+    if (c == '.')
     {
       if (!dot_allowed)
         throw Config::Error(_("Page identifier cannot start with a '.' character or contain two consecutive '.' characters"));
       dot_allowed = false;
     }
-    else if ((*it >= 'a' && *it <= 'z') || (*it >= '0' && *it <= '9') || *it == '_' || *it == '-' || *it == '+')
+    else if (('a' <= c && c <= 'z') || ('0' <= c && c <= '9') || c == '_' || c == '-' || c == '+')
       dot_allowed = true;
     else
       throw Config::Error(_("Page identifier must consist only of letters, digits, '_', '+', '-' and '.' characters"));

djvu-outline.cc
@@ -41,16 +41,16 @@ djvu::OutlineItem& djvu::Outline::add(std::string description, std::string url)
 size_t djvu::OutlineItem::size() const
 {
     size_t size = 1;
-    for (std::vector<djvu::OutlineItem>::const_iterator it = this->children.begin(); it != this->children.end(); it++)
-        size += it->size();
+    for (const djvu::OutlineItem &child : this->children)
+        size += child.size();
     return size;
 }
 
 size_t djvu::Outline::size() const
 {
     size_t size = 0;
-    for (std::vector<djvu::OutlineItem>::const_iterator it = this->items.begin(); it != this->items.end(); it++)
-        size += it->size();
+    for (const djvu::OutlineItem &item : this->items)
+        size += item.size();
     return size;
 }
 
@@ -79,8 +79,8 @@ static void print_int_le(std::ostream &stream, size_t value)
 std::ostream& djvu::operator<<(std::ostream &stream, const djvu::Outline &outline)
 {
     print_int<16>(stream, outline.size());
-    for (std::vector<djvu::OutlineItem>::const_iterator it = outline.items.begin(); it != outline.items.end(); it++)
-        stream << *it;
+    for (const djvu::OutlineItem &item : outline.items)
+        stream << item;
     return stream;
 }
 
@@ -102,8 +102,8 @@ std::ostream& djvu::operator<<(std::ostream &stream, const djvu::OutlineItem &it
     stream << item.description;
     print_int<24>(stream, item.url.size());
     stream << item.url;
-    for (std::vector<djvu::OutlineItem>::const_iterator it = item.children.begin(); it != item.children.end(); it++)
-        stream << *it;
+    for (const djvu::OutlineItem &child : item.children)
+        stream << child;
     return stream;
 }
 

image-filter.cc
@@ -377,10 +377,10 @@ void DefaultQuantizer::operator()(pdf::Renderer *out_fg, pdf::Renderer *out_bg, 
   for (int y = 0; y < height; y++)
   {
     const std::vector<Run> line_runs = runs[y];
-    for (std::vector<Run>::const_iterator run = line_runs.begin(); run != line_runs.end(); run++)
+    for (const Run &run : line_runs)
     {
-      uint32_t color_index = color_map[run->get_color()];
-      write_uint32(stream, ((uint32_t)color_index << 20) + run->get_length());
+      uint32_t color_index = color_map[run.get_color()];
+      write_uint32(stream, ((uint32_t)color_index << 20) + run.get_length());
     }
   }
 }

pdf-backend.cc
@@ -406,18 +406,18 @@ pdf::Metadata::Metadata(pdf::Document &document)
   if (!info.isDict())
     return;
   pdf::Dict *info_dict = info.getDict();
-  for (std::vector<string_field>::iterator it = this->string_fields.begin(); it != this->string_fields.end(); it++)
+  for (auto &field : this->string_fields)
   {
     pdf::OwnedObject object;
-    if (!pdf::dict_lookup(info_dict, it->first, &object)->isString())
+    if (!pdf::dict_lookup(info_dict, field.first, &object)->isString())
       continue;
-    *it->second = pdf::string_as_utf8(object);
+    *(field.second) = pdf::string_as_utf8(object);
   }
-  for (std::vector<date_field>::iterator it = this->date_fields.begin(); it != this->date_fields.end(); it++)
+  for (auto &field : this->date_fields)
   {
     pdf::OwnedObject object;
     char tzs = 0; int tzh = 0, tzm = 0;
-    if (!pdf::dict_lookup(info_dict, it->first, &object)->isString())
+    if (!pdf::dict_lookup(info_dict, field.first, &object)->isString())
       continue;
     char *input = object.getString()->getCString();
     if (input[0] == 'D' && input[1] == ':')
@@ -458,7 +458,7 @@ pdf::Metadata::Metadata(pdf::Document &document)
     }
     if (*input)
       tzh = -1;
-    *it->second = Timestamp(year, month, day, hour, minute, second, tzs, tzh, tzm);
+    *(field.second) = Timestamp(year, month, day, hour, minute, second, tzs, tzh, tzm);
   }
 }
 

pdf-backend.hh
@@ -389,10 +389,10 @@ namespace pdf
     T &extra
   ) const
   {
-    for (std::vector<string_field>::const_iterator it = this->string_fields.begin(); it != this->string_fields.end(); it++)
-      string_callback(it->first, *it->second, extra);
-    for (std::vector<date_field>::const_iterator it = this->date_fields.begin(); it != this->date_fields.end(); it++)
-      date_callback(it->first, *it->second, extra);
+    for (const auto &field : this->string_fields)
+      string_callback(field.first, *field.second, extra);
+    for (const auto &field : this->date_fields)
+      date_callback(field.first, *field.second, extra);
   }
 
 

pdf-document-map.cc
@@ -14,17 +14,17 @@ pdf::DocumentMap::DocumentMap(const std::vector<const char *> &paths)
 {
     int global_index = 0;
     this->byte_size = 0;
-    for (std::vector<const char*>::const_iterator it = paths.begin(); it != paths.end(); it++)
+    for (const char *path : paths)
     {
         this->indices.push_back(global_index);
         {
-            std::ifstream ifs(*it, std::ifstream::in | std::ifstream::binary);
+            std::ifstream ifs(path, std::ifstream::in | std::ifstream::binary);
             ifs.seekg(0, std::ios::end);
             this->byte_size += ifs.tellg();
             ifs.close();
         }
         {
-            std::unique_ptr<pdf::Document> doc(new pdf::Document(*it));
+            std::unique_ptr<pdf::Document> doc(new pdf::Document(path));
             pdf::Catalog *catalog = doc->getCatalog();
             for (int i = 0; i < doc->getNumPages(); i++) {
                 pdf::String s;

pdf2djvu.cc
@@ -220,15 +220,15 @@ protected:
 
   void clean_files()
   {
-    for (std::vector<Component*>::iterator it = this->components.begin(); it != this->components.end(); it++)
+    for (auto &component : this->components)
     {
-      delete *it;
-      *it = nullptr;
+      delete component;
+      component = nullptr;
     }
-    for (std::vector<File*>::iterator it = this->files.begin(); it != this->files.end(); it++)
+    for (auto &file : this->files)
     {
-      delete *it;
-      *it = nullptr;
+      delete file;
+      file = nullptr;
     }
   }
 
@@ -597,15 +597,15 @@ public:
     std::string texts = this->text_comments->str();
     if (config.text_filter_command_line.length() > 0)
       texts = Command::filter(config.text_filter_command_line, texts);
-    for (std::string::iterator it = texts.begin(); it != texts.end(); it++)
-      switch (*it)
+    for (char &c : texts)
+      switch (c)
       {
       case '\x01':
-        *it = '#'; break;
+        c = '#'; break;
       case '\x02':
-        *it = 'T'; break;
+        c = 'T'; break;
       case '\x03':
-        *it = 'x'; break;
+        c = 'x'; break;
       }
     return texts;
   }
@@ -1062,14 +1062,14 @@ void IndirectDjVm::create(const std::vector<Component> &components, bool bare)
       bzz_file.write("\0\0", 3);
     if (shared_ant)
       bzz_file << '\3';
-    for (std::vector<Component>::const_iterator it = components.begin(); it != components.end(); it++)
-      bzz_file << (it->get_title().length() == 0 ? '\001' : '\101');
+    for (const Component &component : components)
+      bzz_file << (component.get_title().length() == 0 ? '\001' : '\101');
     if (shared_ant)
       bzz_file << djvu::shared_ant_file_name << '\0';
-    for (std::vector<Component>::const_iterator it = components.begin(); it != components.end(); it++)
+    for (const Component &component : components)
     {
-      bzz_file << it->get_basename() << '\0';
-      const std::string &title = it->get_title();
+      bzz_file << component.get_basename() << '\0';
+      const std::string &title = component.get_title();
       if (title.length() == 0)
         continue;
       bzz_file << title << '\0';
@@ -1331,10 +1331,8 @@ static int xmain(int argc, char * const argv[])
   }
   if (config.pages.size() == 0)
     config.pages.push_back(std::make_pair(1, n_pages));
-  for (
-    std::vector< std::pair<int, int> >::iterator page_range = config.pages.begin();
-    page_range != config.pages.end(); page_range++)
-  for (int n = page_range->first; n <= n_pages && n <= page_range->second; n++)
+  for (const std::pair<int, int> &page : config.pages)
+  for (int n = page.first; n <= n_pages && n <= page.second; n++)
   {
     static int i = 1;
     if (page_map.get(n, 0))
@@ -1345,11 +1343,11 @@ static int xmain(int argc, char * const argv[])
   }
   {
     std::map<std::string, size_t> known_titles;
-    for (std::vector<int>::const_iterator np = page_numbers.begin(); np != page_numbers.end(); np++)
+    for (int np : page_numbers)
     {
-      Component &component = (*page_files)[*np];
+      Component &component = (*page_files)[np];
       const std::string &title = component.set_title(
-         page_files->get_title(*np, document_map.get(*np).label)
+         page_files->get_title(np, document_map.get(np).label)
       );
       if (title.length() > 0)
       {
@@ -1571,9 +1569,8 @@ static int xmain(int argc, char * const argv[])
           ppm_file << "P6 " << bg_width << " " << bg_height << " 255" << std::endl;
           for (int y = 0; y < bg_height; y++)
           for (int x = 0; x < bg_width; x++)
-          for (int i = 0; i < 3; i++)
+          for (char c : background_color)
           {
-            char c = background_color[i];
             ppm_file.write(&c, 1);
           }
           ppm_file.close();
@@ -1613,8 +1610,8 @@ static int xmain(int argc, char * const argv[])
       debug(3) << _("extracting annotations") << std::endl;
       const std::vector<sexpr::Ref> &annotations = outm->get_annotations();
       sed_file << "select 1" << std::endl << "set-ant" << std::endl;
-      for (std::vector<sexpr::Ref>::const_iterator it = annotations.begin(); it != annotations.end(); it++)
-        sed_file << *it << std::endl;
+      for (const sexpr::Ref &annotation : annotations)
+        sed_file << annotation << std::endl;
       sed_file << "." << std::endl;
       outm->clear_annotations();
     }

commit 435f6a68d059c7eedddc1e4c5bda510893553bd7

sys-command-win32: fix syntax error.
Fixes: 0f36426294ffa136000fabb1dd1973eda61ff7f4

sys-command-win32.cc
@@ -33,7 +33,7 @@ Command::Command(const std::string& command) : command(command)
         if (c == '/')
             stream << '\\';
         else
-            stream << *it;
+            stream << c;
     }
     this->command = stream.str();
     this->argv.push_back(this->command);