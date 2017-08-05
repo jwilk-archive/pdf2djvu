6b40137e61ccc783c28b7c1a4f3335e6bba1a26a
Jakub Wilk <jwilk@jwilk.net>
2017-08-05 18:19:47 +0200

    tests/test-jpeg2000: add.

---
+++ tests/test-jpeg2000.py
@@ -0,0 +1,24 @@
+# encoding=UTF-8
+
+# Copyright © 2017 Jakub Wilk <jwilk@jwilk.net>
+#
+# This file is part of pdf2djvu.
+#
+# pdf2djvu is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# pdf2djvu is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+from tools import (
+    case,
+)
+
+class test(case):
+    def test(self):
+        self.pdf2djvu().assert_()
+
+# vim:ts=4 sts=4 sw=4 et
