jwilk-archive/pdf2djvu

commit 43df875ad12e5b6288b0d48e6df4a3d2e4a7a584

Refactor exception handling to loosen module dependencies.

config.hh
@@ -12,9 +12,9 @@
 #include <sstream> 
 #include <string>
 #include <vector>
+#include <stdexcept>
 
 #include "sexpr.hh"
-#include "debug.hh"
 
 namespace config
 {
@@ -51,10 +51,10 @@ namespace config
   extern char *file_name;
   extern std::string pageid_prefix;
 
-  class Error : public ::Error
+  class Error : public std::runtime_error
   {
   public:
-    explicit Error(const std::string &message) : ::Error(message) {};
+    explicit Error(const std::string &message) : std::runtime_error(message) {};
     virtual bool is_quiet() const
     {
       return false;
@@ -63,7 +63,7 @@ namespace config
     {
       return false;
     }
-    virtual ~Error() { /* just to shut up compilers */ }
+    virtual ~Error() throw() { /* just to shut up compilers */ }
   };
 
   class PagesParseError : public Error
@@ -78,6 +78,12 @@ namespace config
     PageSizeParseError() : Error("Unable to parse page size") {}
   };
 
+  class NoPagesSelected : public Error
+  {
+  public:
+    NoPagesSelected() : Error("No pages selected") { }
+  };
+
   class HyperlinksOptionsParseError : public Error
   {
   public:
@@ -86,13 +92,19 @@ namespace config
 
   class DpiOutsideRange : public Error
   {
-  public:
-    DpiOutsideRange(int dpi_from, int dpi_to) : Error("The specified resolution is outside the allowed range")
+  private:
+    static std::string __error_message__(int dpi_from, int dpi_to)
     {
       std::ostringstream stream;
-      stream << ": " << dpi_from << " .. " << dpi_to;
-      this->message += stream.str();
+      stream
+        << "The specified resolution is outside the allowed range: " 
+        << dpi_from << " .. " << dpi_to;
+      return stream.str();
     }
+  public:
+    DpiOutsideRange(int dpi_from, int dpi_to) 
+    : Error(__error_message__(dpi_from, dpi_to))
+    { }
   };
 
   class NeedHelp : public Error
@@ -119,11 +131,8 @@ namespace config
     }
   };
 
-  class NeedVersion : public ::Error
-  {
-  public:
-    NeedVersion() : Error("") {};
-  };
+  class NeedVersion
+  { };
 
   void read_config(int argc, char * const argv[]);
   void usage(const Error &error);

Makefile
@@ -13,11 +13,11 @@ override CXXFLAGS += $(EXT_CFLAGS) -DDJVULIBRE_BIN_PATH="\"$(strip $(DJVULIBRE_B
 all: pdf2djvu
 
 config.o: config.cc config.hh debug.hh djvuconst.hh sexpr.hh version.hh
-debug.o: debug.cc debug.hh config.hh sexpr.hh
+debug.o: debug.cc debug.hh
 compoppler.o: compoppler.cc compoppler.hh
 sexpr.o: sexpr.cc sexpr.hh
 system.o: system.cc system.hh debug.hh
-quantizer.o: quantizer.cc quantizer.hh compoppler.hh debug.hh config.hh sexpr.hh version.hh
+quantizer.o: quantizer.cc quantizer.hh compoppler.hh config.hh sexpr.hh version.hh
 pdf2djvu.o: pdf2djvu.cc compoppler.hh debug.hh config.hh system.hh version.hh djvuconst.hh sexpr.hh quantizer.hh
 pdf2djvu: pdf2djvu.o compoppler.o debug.o config.o system.o sexpr.o quantizer.o
 	$(LINK.cc) $(^) $(LDLIBS) -o $(@) 

compoppler.cc
@@ -36,16 +36,21 @@ pdf::Environment::Environment()
 #endif
 }
 
-bool pdf::Environment::set_antialias(bool value)
+void pdf::Environment::set_antialias(bool value)
 {
 #if POPPLER_VERSION >= 701 && POPPLER_VERSION < 703
   // Don't allow poppler bug #15009 to appear.
-  // See <https://bugs.freedesktop.org/show_bug.cgi?id=15009> for details.
-  return false;
+  if (!value)
+    throw UnableToSetParameter(
+      "Refused to disable anti-aliasing. "
+      "See <https://bugs.freedesktop.org/show_bug.cgi?id=15009> for details."
+    );
 #endif
-  return globalParams->setAntialias(const_cast<char*>(value ? "yes" : "no"));
+  if (!globalParams->setAntialias(const_cast<char*>(value ? "yes" : "no")))
+    throw UnableToSetParameter("Unable to set antialias parameter");
 #if POPPLER_VERSION >= 509
-  return globalParams->setVectorAntialias(const_cast<char*>(value ? "yes" : "no"));
+  if (!globalParams->setVectorAntialias(const_cast<char*>(value ? "yes" : "no")))
+    throw UnableToSetParameter("Unable to set vector antialias parameter");
 #endif
 }
 
@@ -56,7 +61,10 @@ bool pdf::Environment::set_antialias(bool value)
 
 pdf::Document::Document(const std::string &file_name) 
 : ::PDFDoc(new pdf::String(file_name.c_str()), NULL, NULL)
-{ }
+{ 
+  if (!this->isOk())
+    throw LoadError();
+}
 
 static std::string html_color(const double rgb[])
 {

compoppler.hh
@@ -11,6 +11,7 @@
 #include <string>
 #include <ostream>
 #include <vector>
+#include <stdexcept>
 
 #include <PDFDoc.h>
 #include <GfxState.h>
@@ -274,7 +275,14 @@ namespace pdf
   {
   public:
     Environment();
-    bool set_antialias(bool value);
+    void set_antialias(bool value);
+    class UnableToSetParameter : public std::runtime_error
+    {
+    public:
+      UnableToSetParameter(const std::string &message)
+      : std::runtime_error(message)
+      { }
+    };
   };
 
 
@@ -288,6 +296,13 @@ namespace pdf
     Document(const std::string &file_name);
     void display_page(Renderer *renderer, int npage, double hdpi, double vdpi, bool crop, bool do_links);
     void get_page_size(int n, bool crop, double &width, double &height);
+    class LoadError : public std::runtime_error
+    {
+    public:
+      LoadError()
+      : std::runtime_error("Unable to load document")
+      { }
+    };
   };
 
 

config.cc
@@ -7,6 +7,7 @@
 
 #include <stdexcept>
 
+#include "debug.hh"
 #include "config.hh"
 #include "djvuconst.hh"
 #include "version.hh"
@@ -306,11 +307,12 @@ void config::read_config(int argc, char * const argv[])
 
 void config::usage(const config::Error &error)
 {
+  std::ostream &log = debug(0, config::verbose);
   if (error.is_already_printed())
-    debug(0) << std::endl;
+    log << std::endl;
   if (!error.is_quiet())
-    debug(0) << error << std::endl << std::endl;
-  debug(0) 
+    log << error << std::endl << std::endl;
+  log
     << "Usage: " << std::endl
     << "   pdf2djvu [-o <output-djvu-file>] [options] <pdf-file>" << std::endl
     << "   pdf2djvu  -i <index-djvu-file>   [options] <pdf-file>" << std::endl

debug.cc
@@ -6,21 +6,15 @@
  */
 
 #include "debug.hh"
-#include "config.hh"
 
 DevNull dev_null;
 
-std::ostream &debug(int n)
+std::ostream &debug(int n, int threshold)
 {
-  if (n <= config::verbose)
+  if (n <= threshold)
     return std::clog;
   else
     return dev_null;
-}
-
-std::ostream &operator<<(std::ostream &stream, const Error &error)
-{
-  return stream << error.message;
 }
 
 // vim:ts=2 sw=2 et

debug.hh
@@ -6,11 +6,12 @@
  */
 
 #include <iostream>
+#include <stdexcept>
 
 #ifndef PDF2DJVU_DEBUG_H
 #define PDF2DJVU_DEBUG_H
 
-std::ostream &debug(int n);
+std::ostream &debug(int n, int threshold);
 
 class DevNull : public std::ostream
 {
@@ -20,15 +21,10 @@ public:
 
 extern DevNull dev_null;
 
-class Error
+static inline std::ostream &operator<<(std::ostream &stream, const std::runtime_error &error)
 {
-public:
-  Error() : message("Unknown error") {};
-  explicit Error(const std::string &message) : message(message) {};
-  friend std::ostream &operator<<(std::ostream &, const Error &);
-protected:
-  std::string message;
-};
+  return stream << error.what();
+}
 
 #endif
 

pdf2djvu.cc
@@ -27,6 +27,11 @@
 #include "sexpr.hh"
 #include "quantizer.hh"
 
+static inline std::ostream &debug(int n)
+{
+  return debug(n, config::verbose);
+}
+
 static std::string text_comment(int x, int y, int dx, int dy, int w, int h, const Unicode *unistr, int len)
 {
   std::ostringstream strstream;
@@ -51,10 +56,10 @@ static std::string text_comment(int x, int y, int dx, int dy, int w, int h, cons
   return strstream.str();
 }
 
-class NoLinkDestination : public Error
+class NoLinkDestination : public std::runtime_error
 {
 public:
-  NoLinkDestination() : Error("Cannot find link destination") {}
+  NoLinkDestination() : std::runtime_error("Cannot find link destination") {}
 };
 
 static int get_page_for_LinkGoTo(pdf::link::GoTo *goto_link, pdf::Catalog *catalog)
@@ -336,10 +341,10 @@ public:
   }
 };
 
-class BookmarkError : public Error
+class BookmarkError : public std::runtime_error
 {
 public:
-  BookmarkError(const std::string &message) : Error(message) {}
+  BookmarkError(const std::string &message) : std::runtime_error(message) {}
 };
 
 class NoPageForBookmark : public BookmarkError
@@ -449,7 +454,13 @@ static void pdf_outline_to_djvu_outline(pdf::Document *doc, std::ostream &stream
   stream << expr;
 }
 
-class InvalidDateFormat : public Error { };
+class InvalidDateFormat : public std::runtime_error
+{ 
+public:
+  InvalidDateFormat()
+  : std::runtime_error("Invalid date format")
+  { }
+};
 
 static int scan_date_digits(char * &input, int n)
 {
@@ -698,6 +709,13 @@ private:
   File &index_file;
   File *outline_sed_file;
   std::vector<std::string> components;
+  class UnexpectedDjvuSedOutput : public std::runtime_error
+  { 
+  public: 
+    UnexpectedDjvuSedOutput() 
+    : std::runtime_error("Unexpected output from djvused") 
+    { };
+  };
 public:
   explicit IndirectDjVm(File &index_file) 
   : index_file(index_file),
@@ -764,7 +782,7 @@ public:
           this->index_file << static_cast<char>((chunks_size >> (8 * i)) & 0xff);
       }
       else
-        throw Error("Unexpected output from djvused");
+        throw UnexpectedDjvuSedOutput();
     }
   }
 
@@ -817,6 +835,14 @@ static int calculate_dpi(double page_width, double page_height)
     return config::dpi;
 }
 
+class StdoutIsATerminal : public std::runtime_error
+{
+public:
+  StdoutIsATerminal() 
+  : std::runtime_error("I won't write DjVu data to a terminal.")
+  { }
+};
+
 static int xmain(int argc, char * const argv[])
 {
   std::ios_base::sync_with_stdio(false);
@@ -837,11 +863,10 @@ static int xmain(int argc, char * const argv[])
   }
 
   if (config::output_stdout && is_stream_a_tty(std::cout))
-    throw Error("I won't write DjVu data to a terminal.");
+    throw StdoutIsATerminal();
 
   pdf::Environment environment;
-  if (!environment.set_antialias(config::antialias))
-    throw Error("Unable to set antialias parameter");
+  environment.set_antialias(config::antialias);
 
   size_t pdf_size;
   { 
@@ -852,8 +877,6 @@ static int xmain(int argc, char * const argv[])
   }
 
   pdf::Document *doc = new pdf::Document(config::file_name);
-  if (!doc->isOk())
-    throw Error("Unable to load document");
 
   pdf::splash::Color paper_color;
   pdf::set_color(paper_color, 0xff, 0xff, 0xff);
@@ -996,9 +1019,9 @@ static int xmain(int argc, char * const argv[])
       debug(3) << "  - subsampled render" << std::endl;
       doc->display_page(outs, n, hdpi, vdpi, crop, true);
       if (sub_width != outs->getBitmapWidth())
-        throw Error("Unexpected subsampled bitmap width");
+        throw std::logic_error("Unexpected subsampled bitmap width");
       if (sub_height != outs->getBitmapHeight())
-        throw Error("Unexpected subsampled bitmap height");
+        throw std::logic_error("Unexpected subsampled bitmap height");
       pdf::Pixmap bmp(outs);
       debug(3) << "  - background pixmap >> sep_file" << std::endl;
       sep_file << "P6 " << sub_width << " " << sub_height << " 255" << std::endl;
@@ -1138,7 +1161,7 @@ static int xmain(int argc, char * const argv[])
     }
   }
   if (page_counter == 0)
-    throw Error("No pages selected");
+    throw config::NoPagesSelected();
   {
     TemporaryFile dummy_page_file;
     TemporaryFile sed_file;
@@ -1229,15 +1252,15 @@ int main(int argc, char **argv)
   {
     xmain(argc, argv);
   }
-  catch (Error &ex)
-  {
-    std::cerr << ex << std::endl;
-    exit(1);
-  }
   catch (std::ios_base::failure &ex)
   {
     std::cerr << "I/O error (" << ex.what() << ")" << std::endl;
     exit(2);
+  }
+  catch (std::runtime_error &ex)
+  {
+    std::cerr << ex << std::endl;
+    exit(1);
   }
 }
 

commit 61a16afa275aeef1eecd8766f6af7a3f4aa79d18

config: remove spurious destructor for Error.
Fixes: 43df875ad12e5b6288b0d48e6df4a3d2e4a7a584

config.hh
@@ -107,7 +107,6 @@ public:
     {
       return false;
     }
-    virtual ~Error() throw () { /* just to silence compilers */ }
   };
 
   class NoPagesSelected : public Error