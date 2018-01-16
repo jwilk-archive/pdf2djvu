jwilk-archive/pdf2djvu

commit 6ad06457299d10e75fd43beb1964262481cde645

Fix manual page date format.

tools/manpage.xsl
@@ -1,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+    <xsl:import href="/usr/share/xml/docbook/stylesheet/docbook-xsl/manpages/docbook.xsl"/>
+    <xsl:param name="local.l10n.xml" select="document('')"/>
+    <l:i18n xmlns:l="http://docbook.sourceforge.net/xmlns/l10n/1.0">
+        <l:l10n language="en">
+            <l:context name="datetime"><l:template name="format" text="Y-m-d"/></l:context>
+        </l:l10n>
+        <l:l10n language="pl">
+            <l:context name="datetime"><l:template name="format" text="d.m.Y"/></l:context>
+        </l:l10n>
+        <l:l10n language="ru">
+            <l:context name="datetime"><l:template name="format" text="d.m.Y"/></l:context>
+        </l:l10n>
+    </l:i18n>
+</xsl:stylesheet>
+
+<!-- vim:set ts=4 sts=4 sw=4 tw=120 et: -->

tools/build-manpage
@@ -36,7 +36,7 @@ done
 [ $# -eq 2 ] || { usage; exit 1; }
 src="$1"
 dst="$2"
-xsl=http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl
+xsl="$here/manpage.xsl"
 xmllint='xmllint --valid --noout --nonet'
 xsltproc='xsltproc --nonet --param man.charmap.use.subset 0'
 [ -n "$opt_lang" ] && xsltproc="$xsltproc --param man.output.lang.in.name.enabled 1"

commit 59e7c09f2a8f9f0b4683fe021069d21a25fa692c

tools/manpage.xsl: use URL to identify DocBook XSL.
Fixes: 6ad06457299d10e75fd43beb1964262481cde645

tools/manpage.xsl
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
-    <xsl:import href="/usr/share/xml/docbook/stylesheet/docbook-xsl/manpages/docbook.xsl"/>
+    <xsl:import href="http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl"/>
     <xsl:param name="local.l10n.xml" select="document('')"/>
     <l:i18n xmlns:l="http://docbook.sourceforge.net/xmlns/l10n/1.0">
         <l:l10n language="en">