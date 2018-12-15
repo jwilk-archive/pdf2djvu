1200116e608bb9612934f8081ff021579665cd36
Jakub Wilk <jwilk@jwilk.net>
2018-12-15 16:11:42 +0100

    win32: upgrade Poppler to 0.72.0.

---
+++ win32/misc/dummy-mutex.hh
@@ -0,0 +1,39 @@
+/* Copyright © 2018 Jakub Wilk <jwilk@jwilk.net>
+ *
+ * This file is part of pdf2djvu.
+ *
+ * pdf2djvu is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * pdf2djvu is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef PDF2DJVU_DUMMY_MUTEX
+#define PDF2DJVU_DUMMY_MUTEX
+
+#include <mutex>
+
+#if !defined(_GLIBCXX_HAS_GTHREADS)
+
+namespace std {
+
+    class recursive_mutex {
+    public:
+        void lock() { }
+        void unlock() { }
+        bool try_lock() {
+            return true;
+        }
+    };
+
+}
+
+#endif
+
+#endif
+
+// vim:ts=4 sts=4 sw=4 et
